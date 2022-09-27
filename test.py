e66c3ff4331f7929a61b61766a5c4366f7123099
ItaloSS <83152601+ItaloSS@users.noreply.github.com>
2022-09-27 17:21:21 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def oscilador_forzado(X,t):
     alpha = 1.0
-    gamma = 1.0
+    gamma = 0.1/(9.8**2)
     omega = 1.0
     Y = X.copy()
     #en X[0] tenemos la posicion(inicial en un principio)
@@ -12,7 +12,7 @@ def oscilador_forzado(X,t):
     #en Y[0] se guarda la posicion, como varia es en funcion de la velocidad
     #en Y[1] se guarda la velocidad, como varia es en funcion de la funcion aceleracion
     Y[0] = X[1]
-    Y[1] = gamma*np.cos(omega*t) -alpha*X[0]
+    Y[1] = +gamma*X[1] - np.sin(X[0]) + np.sin(omega*t)*np.cos(X[0])*(1/9.8)
     return Y
 def pol(Y,t):
     # dx/dt = 5t
@@ -23,7 +23,7 @@ def pol(Y,t):
 
 
 
-osf = nm.Num_meth(oscilador_forzado,0.005,0.0,30)
-cond_iniciales = np.array([0,1])
-osf.RK4(cond_iniciales)
+osf = nm.Num_meth(oscilador_forzado,0.05,0.0,1000)
+cond_iniciales = np.array([0,0.1])
+osf.RK4(cond_iniciales,True)
 
